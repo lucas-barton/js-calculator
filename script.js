ebcefc013c14a8a73070df68f35da5b90ff3d8b6
Lucas <lucas.m.barton@gmail.com>
2022-10-23 01:42:32 -0400

    digit and decimal to display

---
+++ script.js
@@ -0,0 +1,61 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null
+};
+
+function inputDigit(digit) {
+    const { displayValue } = calculator;
+    // Overwrite 'displayValue' if the current value is '0' otherwise append to it
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+}
+
+function inputDecimal(dot) {
+    // If the 'displayValue' property does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+        // Append the decimal point
+        calculator.displayValue += dot;
+    }
+}
+
+
+function updateDisplay() {
+    // select the element with class of 'calculator-screen'
+    const display = document.querySelector('.calculator-screen');
+    //update the value of the element with the contents of 'displayValue'
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    // Access the clicked element
+    const { target } = event;
+
+    // Check if the clicked element is a button.
+    // If not, exit from the function
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;''
+    }
+
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+})
